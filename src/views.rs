c2ed2a3a29de6e28c5eb970cb6cbd25ed7e51493
andornaut <andornaut@users.noreply.github.com>
2023-09-21 11:48:15 -0400

    Notify in status bar after copy/cut

--- src/views.rs
+++ src/views.rs
@@ -19,7 +19,8 @@ use ratatui::{
 };
 use unicode_segmentation::UnicodeSegmentation;
 
-const ELLIPSIS: &str = "\n…";
+const ELLIPSIS: &str = "…";
+const NEWLINE_ELLIPSIS: &str = "\n…";
 
 pub(super) trait View<B: Backend>: CommandHandler {
     fn render(&mut self, frame: &mut Frame<B>, rect: Rect, mode: &InputMode, theme: &Theme);
@@ -45,7 +46,7 @@ pub(super) fn bordered<B: Backend>(
 pub(super) fn split_with_ellipsis(line: &str, width: u16) -> Vec<String> {
     assert!(width > 0);
 
-    let split = split_utf8_with_reservation(&line, width, ELLIPSIS);
+    let split = split_utf8_with_reservation(&line, width, NEWLINE_ELLIPSIS);
     let mut lines = Vec::new();
     let mut it = split.into_iter().peekable();
     while let Some(part) = it.next() {
@@ -58,11 +59,16 @@ pub(super) fn split_with_ellipsis(line: &str, width: u16) -> Vec<String> {
 
 fn split_utf8_with_reservation(line: &str, width: u16, reservation: &str) -> Vec<String> {
     if len_utf8(line) <= width {
-        return vec![line.to_string()];
+        return vec![line.into()];
     }
 
-    let reserved = len_utf8(reservation);
-    split_utf8(line, width.saturating_sub(reserved))
+    let reserved_len = len_utf8(reservation);
+    let width = width.saturating_sub(reserved_len);
+    if width == 0 {
+        return Vec::new();
+    }
+
+    split_utf8(line, width.saturating_sub(reserved_len))
 }
 
 fn len_utf8(line: &str) -> u16 {
@@ -75,4 +81,44 @@ fn split_utf8(line: &str, width: u16) -> Vec<String> {
         .map(|_| graphemes.by_ref().take(width as usize).collect::<String>())
         .take_while(|s| !s.is_empty())
         .collect::<Vec<_>>()
+}
+
+pub(super) fn truncate_left_utf8_with_ellipsis(line: &str, width: u16) -> String {
+    if len_utf8(line) <= width {
+        return line.into();
+    }
+
+    let reserved_len = len_utf8(ELLIPSIS);
+    let width = width.saturating_sub(reserved_len);
+    if width == 0 {
+        return "".into();
+    }
+
+    let mut graphemes = UnicodeSegmentation::graphemes(line, true);
+    let mut chars = graphemes
+        .by_ref()
+        .rev()
+        .take(width as usize)
+        .collect::<Vec<&str>>();
+    chars.reverse();
+    format!("{ELLIPSIS}{}", chars.join(""))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_case::test_case;
+
+    #[test_case("example".to_string(),  "example", 7; "full width")]
+    #[test_case("…ample".to_string(),  "example", 6; "width -1")]
+    #[test_case("…e".to_string(),  "example", 2; "2 width")]
+    #[test_case("e".to_string(),  "e", 1; "1 width but full")]
+    #[test_case("".to_string(),  "example", 1; "1 width")]
+    #[test_case("".to_string(),  "example", 0; "0 width")]
+    #[test_case("".to_string(),  "", 4; "empty string")]
+    fn truncate_left_utf8_with_ellipsis_is_correct(expected: String, line: &str, width: u16) {
+        let result = truncate_left_utf8_with_ellipsis(line, width);
+
+        assert_eq!(expected, result);
+    }
 }
